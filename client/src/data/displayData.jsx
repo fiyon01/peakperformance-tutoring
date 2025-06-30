7106d9ab5ca16e3d010bfc8e440a69eb842f39bc
fiyon01 <147448591+fiyon01@users.noreply.github.com>
2025-07-01 02:14:31 +0300

    Update displayData.jsx

--- client/src/data/displayData.jsx
+++ client/src/data/displayData.jsx
@@ -42,7 +42,7 @@ export const testimonials = [
       "/images/methodsImages (1).jpg",
       '/images/Screenshot_1-7-2025_03238_www.tiktok.com.jpeg',
       "https://scontent.fnuu2-1.fna.fbcdn.net/v/t39.30808-6/475539602_1184209480037578_3269351206353649665_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=aa7b47&_nc_ohc=WQ-h4crFb8AQ7kNvwGHHYj-&_nc_oc=AdnTNEWCiW1Kf3L15QQrKxSiX3hS9qehh7SD3EX9IRiFRjr2GEXIBz8H-ye-yJ0P4-g&_nc_zt=23&_nc_ht=scontent.fnuu2-1.fna&_nc_gid=P9zVF-iM4T0auL3STIdvnQ&oh=00_AfNeuiYKxNg7SM7lJGBKpaeVCujvnHCE7GDkFMv0u47xtA&oe=6868D622",
-       "/images/WhatsApp Image 2025-04-11 at 6.29.11 PM.jpeg",
+       "/images/WhatsApp Image 2025-04-11 at 6.29.14 PM (1).jpeg",
       "/images/Screenshot_1-7-2025_03620_www.tiktok.com.jpeg",
       "/images/Screenshot_1-7-2025_03657_www.tiktok.com.jpeg",
      
